8a3590d1bc2630bb0228bc00a22ebe87b83ebbb7
Sebamon2040 <sebastianmonteirop@gmail.com>
2023-09-04 21:11:08 -0300

    Cambios al volante

--- scenes/car.gd
+++ scenes/car.gd
@@ -1,5 +1,26 @@
 extends VehicleBody3D
 
+var isTurningLeft 
+var isTurningRight 
+@onready var volante = %Volante
+var turningRate = 0.3
 
+func _ready() -> void:
+	isTurningLeft = false
+	isTurningRight = false
 
-
+func _process(delta):
+	
+	if Input.is_action_pressed("turn_left"):
+		isTurningLeft = true
+		isTurningRight = false
+	elif Input.is_action_pressed("turn_right"):
+		isTurningRight = true
+		isTurningLeft = false
+	else:
+		isTurningLeft = false
+		isTurningRight = false
+	
+	if not isTurningLeft && not isTurningRight:
+		pass
+		
