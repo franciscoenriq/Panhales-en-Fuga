5b25a04a6709a10cffa5dc5c42ddd61a23503bb4
montivl <vmontoya@ug.uchile.cl>
2023-12-14 12:08:09 -0300

    LAST UPDATE YUJUUUUU

--- scripts/car_npc.gd
+++ scripts/car_npc.gd
@@ -34,7 +34,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var npc_speed
 	var objetivo
 	var new_position
